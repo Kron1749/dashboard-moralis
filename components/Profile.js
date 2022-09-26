98a3e00cb39ab1e356423f182e9b620fc2021e97
Kron1749 <drakgr98@gmail.com>
2022-09-26 11:24:23 +0300

    Updated form

--- components/Profile.js
+++ components/Profile.js
@@ -1,10 +1,29 @@
 import CustomContainer from "./CustomContainer"
-import { Text } from "@chakra-ui/react"
+import { Button, FormControl, FormLabel, Text,Input } from "@chakra-ui/react"
+import { useState } from "react"
+import { useMoralis } from "react-moralis"
 
 export default function Profile({ user }) {
+    const [input,setInput] = useState("")
+    const {setUserData,isUserUpdating} = useMoralis()
     return (
-        <CustomContainer>
-            <Text>Username: {user.getUsername} </Text>
+        <CustomContainer>            
+            <Text>Username: {user.getUsername()} </Text>
+            <Text>Wallet address: {user.get('ethAddress')} </Text>
+            <form onSubmit={e=>{
+                e.preventDefault()
+                if(input.trim() !== ''){
+                    setUserData({
+                        username: input
+                    }).then(()=>setInput(''))
+                }
+            }}>
+                <FormControl mt="6" mb="6">
+                    <FormLabel htmlFor="username">Set a new username</FormLabel>
+                    <Input id="username" type="text" placeholder="your name" value={input} onChange={e=>setInput(e.target.value)}/>
+                </FormControl>
+                <Button type="submit" colorScheme="purple" disabled={isUserUpdating}>Change Username</Button>
+            </form>
         </CustomContainer>
     )
 }
